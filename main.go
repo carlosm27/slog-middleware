198aa89ef5dae4bcd962acfd2b4a1af1c5ae9e84
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-08-26 20:33:33 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,17 +8,17 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	port := ":8080"
+	port := ":8000"
 
-	// Register the routes and handlers
+	logger := slog.Default()
 	finalHandler := http.HandlerFunc(homeHandler)
 	mux.Handle("/", LoggingMiddleware(finalHandler))
 
-	slog.Info("Listening on ", "port", port)
+	logger.Info("Listening on ", "port", port)
 
 	err := http.ListenAndServe(port, mux)
 	if err != nil {
-		slog.Warn("Problem starting the server", "error", err)
+		logger.Error("Problem starting the server", "error", err)
 	}
 
 }
@@ -31,11 +31,20 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//var resp http.Response
+
 		logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 		logger.Info("Request Info",
 			slog.String("method", r.Method),
 			slog.String("path", r.RequestURI),
+			//slog.String("status", StatusCode),
 		)
 		next.ServeHTTP(w, r)
+		statusCode := w.Header().Get("Status")
+		logger.Info("Request Info",
+			slog.String("method", r.Method),
+			slog.String("path", r.RequestURI),
+			slog.String("status", statusCode),
+		)
 	})
 }
